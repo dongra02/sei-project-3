724a294e7e53d5d0bf5fea0d1bd038e3a217b1dc
renato76 <renmignogna@gmail.com>
2020-10-01 15:01:41 +0100

    finished frst version of navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,8 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Home from './components/common/Home'
-<<<<<<< HEAD
-import RouteIndex from './components/Routes/RouteIndex'
 import Navbar from './components/common/Navbar'
-=======
 import QuestIndex from './components/Quests/QuestIndex'
->>>>>>> development
 
 
 
@@ -18,15 +14,11 @@ const App = () => {
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
-<<<<<<< HEAD
-        <Route path="/quests" component={RouteIndex} />
-        <Route path="/create" component={RouteIndex} />
+        <Route path="/quests" component={QuestIndex} />
+        {/* <Route path="/create" component={RouteIndex} />
         <Route path="/register" component={RouteIndex} />
         <Route path="/login" component={RouteIndex} />
-        <Route path="/profile" component={RouteIndex} />
-=======
-        <Route path="/quests" component={QuestIndex} />
->>>>>>> development
+        <Route path="/profile" component={RouteIndex} /> */}
       </Switch>
       {/* Footer */}
     </BrowserRouter>
