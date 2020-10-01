687988c7ddef9a778b336cebbf35615a66746baf
renato76 <renmignogna@gmail.com>
2020-10-01 13:33:42 +0100

    added navbar

--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -3,14 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar navbar-dark bg-primary">
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <nav className="navbar navbar-expand-lg navbar navbar-dark bg-primary">   
         <div className="navbar-nav">
-          <Link to="/routes" className="nav-item nav-link active">Home <span className="sr-only">(current)</span></Link>
-          <Link to="" className="nav-item nav-link">Browse Routes</Link>
+          <Link to="/" className="nav-item nav-link active">Home <span className="sr-only">(current)</span></Link>
+          <Link to="/routes" className="nav-item nav-link">Browse Routes</Link>
           <Link to="" className="nav-item nav-link">Create Routes</Link>
-        </div>
-      </div>
+        </div>   
     </nav>
   )
 }
