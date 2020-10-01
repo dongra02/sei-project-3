24cd5f748eea85fbc89d3ce7a7950ef069d4dd74
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-10-02 00:43:08 +0300

    minimal stlying on filter component, plus some work on basic map functions that interact with filter and results

--- frontend/src/components/Quests/QuestIndex.js
+++ frontend/src/components/Quests/QuestIndex.js
@@ -1,36 +1,69 @@
 import React from 'react'
 
 import Header from '../common/Header'
+import Filter from '../common/Filter'
 import Map from '../map/Map'
 
 class QuestIndex extends React.Component {
 
   state = {
-    searchResults: [
-      'quest 16345',
-      'quest 56434',
-      'quest 56424',
-      'quest 23754',
-      'quest 08655',
-      'quest 08655',
-      'quest 08655',
-      'quest 08655'
-    ]
+    searchResults: [],
+    formData: {
+      location: 'under the sea',
+      theme: '',
+      time: 0
+    }
+  }
+
+  componentDidMount = () => {
+    this.generateMarkers()
+  }
+
+  generateMarkers = () => {
+    const { searchResults } = this.state
+    for (let i = 0; i < 20; i++) {
+      const lat = 51.51 + (Math.random() / 10)
+      const lng = 0.13 + (Math.random() / 10)
+      searchResults.push({ name: 'questx', lat, lng })
+    }
+
+    this.setState({ searchResults })
+  }
+
+  handleChange = event => {
+    console.log(event.target)
+    const formData = {
+      ...this.state.formData,
+      [event.target.id]: event.target.value
+    }
+
+    this.setState({ formData })
+  }
+
+  getBounds = bounds => {
+    // console.log(bounds)
+  }
+
+  flyToQuest = quest => {
+    console.log(quest)
   }
 
   render() {
-    const { searchResults } = this.state
+    const { formData, searchResults } = this.state
     return (
       <>
         <Header />
         <div className="browse-quests">
-          <div className="filters">FILTERS</div>
+          <h3>Find a new Quest</h3>
+          <Filter {...formData} handleChange={this.handleChange} />
           <div className="results">
             <div className="results-map">
-              <Map />
+              <Map getBounds={this.getBounds} searchResults={searchResults} />
             </div>
             <div className="results-list">
-              {searchResults && searchResults.map((quest, i) => <div key={i} className="results-list-item">{quest}</div>)}
+              <div className="container">
+                {searchResults && searchResults.map((quest, i) => <div key={i} className="results-list-item" onClick={() => this.flyToQuest(quest)}>{quest.name}</div>)}
+              </div>
             </div>
           </div>
         </div>
