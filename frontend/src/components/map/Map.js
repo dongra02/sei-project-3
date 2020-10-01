19c93f351093c2d3e173a667357a3d867345fdb1
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-10-01 18:27:34 +0300

    implement react-map-gl into standalone component and added to browse page

--- frontend/src/components/map/Map.js
+++ frontend/src/components/map/Map.js
@@ -1,39 +1,52 @@
 import React from 'react'
 
 import MapGL from 'react-map-gl'
-import 'mapbox-gl/dist/mapbox-gl.css'
+import 'mapbox-gl/dist/mapbox-gl.css'  
 
 class Map extends React.Component {
 
   state = {
-    zoom: 10,
+    zoom: 5,
     currentLocation: {
-      lat: 0,
-      lng: 0
+      latitude: 0,
+      longitude: 0
     }
+  }
+
+  moveMapView = (event) => {
+    if (event.zoom === this.state.zoom) {
+      const { latitude, longitude } = event
+      this.setState({
+        currentLocation: { latitude, longitude }
+      })
+    }
+  }
+
+  scrollToZoom = event => {
+    const scroll = event.srcEvent.deltaY
+    event.lngLat = [this.state.currentLocation.longitude, this.state.currentLocation.latitude]
+    if (scroll > 5) this.setState({ zoom: this.state.zoom - 0.05 })
+    if (scroll < -5) this.setState({ zoom: this.state.zoom + 0.05 })
   }
 
   render() {
 
     const { zoom, currentLocation } = this.state
 
-    console.log(process.env.REACT_APP_MAPBOX_ACCESS_TOKEN)
-
     return (
       <MapGL
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-        width={'100vw'}
-        height={'100vh'}
+        width={'100%'}
+        height={'100%'}
         mapStyle='mapbox://styles/sriramsiv/ckfqhispj079919t8xxbwa6t7'
-        latitude={currentLocation.lat}
-        longitude={currentLocation.lng}
+        latitude={currentLocation.latitude}
+        longitude={currentLocation.longitude}
         zoom={zoom}
         doubleClickZoom={false}
         onViewportChange={this.moveMapView}
         onDblClick={this.placeMarker}
         getCursor={(() => 'arrow')}
         onWheel={this.scrollToZoom}
-
       />
     )
   }
