776a3cd6c1930b9e0489a87ef69c768776dcb118
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-10-01 16:43:12 +0300

    created basic wireframe for QuestIndex component

--- frontend/src/components/Quests/QuestIndex.js
+++ frontend/src/components/Quests/QuestIndex.js
@@ -5,13 +5,31 @@ import Header from '../common/Header'
 class QuestIndex extends React.Component {
 
   state = {
-
+    searchResults: [
+      'quest 16345',
+      'quest 56434',
+      'quest 56424',
+      'quest 23754',
+      'quest 08655'
+    ]
   }
 
   render() {
+    const { searchResults } = this.state
     return (
       <>
         <Header />
+        <div className="browse-quests">
+          <div className="filters">FILTERS</div>
+          <div className="results">
+            <div className="results-map">
+              MAP
+            </div>
+            <div className="results-list">
+              {searchResults && searchResults.map((quest, i) => <div key={i} className="results-list-item">{quest}</div>)}
+            </div>
+          </div>
+        </div>
       </>
     )
   }
--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from './Header'
 
 const Home = () => {
   return (
     <>
+      <Header />
       <div>Home</div>
     </>
   )
