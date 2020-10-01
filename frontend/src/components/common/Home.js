a888063ef0d757fb29675d5c66ff2dc621f1149b
renato76 <renmignogna@gmail.com>
2020-10-01 14:13:56 +0100

    updated navbar

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home Content</div>
+    <div>Home</div>
   )
 }
 
--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -6,8 +6,10 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar navbar-dark bg-primary">   
         <div className="navbar-nav">
           <Link to="/" className="nav-item nav-link active">Home <span className="sr-only">(current)</span></Link>
-          <Link to="/routes" className="nav-item nav-link active">Browse Routes</Link>
+          <Link to="/quests" className="nav-item nav-link active">Browse Routes</Link>
           <Link to="" className="nav-item nav-link active">Create Routes</Link>
+          <Link to="/register" className="nav-item nav-link active">Register</Link>
+          <Link to="/login" className="nav-item nav-link active">Login</Link>
         </div>   
     </nav>
   )
