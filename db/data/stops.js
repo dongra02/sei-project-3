d75f62f49ce4c4496d3d53bd837f163d8713ae6a
renato76 <renmignogna@gmail.com>
2020-10-02 09:55:47 +0100

    updated

--- db/data/stops.js
+++ db/data/stops.js
@@ -1,46 +1,46 @@
 module.exports = [
   {
-    "name": "London Bridge Railway",
-    "location": {
-      "latitude": 51.505347,
-      "longitude": -0.086167
+    'name': 'London Bridge Railway',
+    'location': {
+      'latitude': 51.505347,
+      'longitude': -0.086167
     },
-    "question": {
-      "clue": "Interesting architecture when seen from below",
-      "answer": "The Shard"
+    'question': {
+      'clue': 'Interesting architecture when seen from below',
+      'answer': 'The Shard'
     }
   },
   {
-    "name": "The Shard",
-    "location": {
-      "latitude": 51.504527,
-      "longitude": -0.086525
+    'name': 'The Shard',
+    'location': {
+      'latitude': 51.504527,
+      'longitude': -0.086525
     },
-    "question": {
-      "clue": "Heritage sales made in my shadow",
-      "answer": "Borough market"
+    'question': {
+      'clue': 'Heritage sales made in my shadow',
+      'answer': 'Borough market'
     }
   },
   {
-    "name": "Borough market",
-    "location": {
-      "latitude": 51.505360,
-      "longitude": -0.090371
+    'name': 'Borough market',
+    'location': {
+      'latitude': 51.505360,
+      'longitude': -0.090371
     },
-    "question": {
-      "clue": "Use this to collect leaves",
-      "answer": "The Rake"
+    'question': {
+      'clue': 'Use this to collect leaves',
+      'answer': 'The Rake'
     }
   },
   {
-    "name": "The Rake",
-    "location": {
-      "latitude": 51.505347,
-      "longitude": -0.086167
+    'name': 'The Rake',
+    'location': {
+      'latitude': 51.505347,
+      'longitude': -0.086167
     },
-    "question": {
-      "clue": "Use this to collect leaves",
-      "answer": "The Rake"
+    'question': {
+      'clue': 'Use this to collect leaves',
+      'answer': 'The Rake'
     }
   }
 ]
