682c58d6bbbb002edf1f00580b081ecbb3944025
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-10-02 14:25:39 +0300

    added tabs to quest page

--- frontend/src/components/Quests/QuestShow.js
+++ frontend/src/components/Quests/QuestShow.js
@@ -6,7 +6,9 @@ import Map from '../map/Map'
 
 class QuestShow extends React.Component {
   state = {
-    screen: null
+    screen: 'map',
+
+    currentStop: 0
   }
 
 
@@ -14,47 +16,51 @@ class QuestShow extends React.Component {
   // Then in render, return the right state according to which button clicked
 
   handleClick = event => {
-    event.preventDefault()
-    const buttonClicked = event.target.innerHTML
-    console.log(buttonClicked)
-    
     this.setState({
-      screen: buttonClicked
+      screen: event.target.value
     })
   }
 
+  nextStop = () => {
+    this.setState({ currentStop: this.currentStop + 1 })
+  }
 
-  render()  {
+
+  render() {
+    const { screen, currentStop } = this.state
     return (
       <>
-      <Header />
-      <div className="show-quests">
-        <div className="top-show-buttons">
-        <button value="map" onClick={this.handleClick} type="button" className="btn btn-primary btn-block">VIEW MAP</button>
-        <button value="clue" onClick={this.handleClick} type="button" className="btn btn-success btn-block">CLUE</button>
-        <button value-="comments" onClick={this.handleClick} type="button" className="btn btn-warning btn-block">COMMENTS</button>
-        </div>
-        <div className="middle">
-          <div className="clues">
-            <h2>Quest</h2>
-            <p>Your next location is:</p>
-            <p>Crown Inn Pub</p>
-            <div className="answer-input">
-              {/* <h2>Answer Input</h2> */}
-              <input>
-              </input>
-              <button type="button" className="btn btn-info">SUBMIT ANSWER</button>
-            </div>
-            <div className="start-button">
-            <button type="button" className="btn btn-success btn-lg">START</button>
-            <button type="button" className="btn btn-danger btn-lg">GO TO NEXT STAGE</button>
-            </div>
+        <Header />
+        <div className="show-quests">
+          <div className="top-show-buttons">
+            <button value="map" onClick={this.handleClick} type="button" className="btn btn-primary btn-block">VIEW MAP</button>
+            <button value="clue" onClick={this.handleClick} type="button" className="btn btn-success btn-block">CLUE</button>
+            <button value="comments" onClick={this.handleClick} type="button" className="btn btn-warning btn-block">COMMENTS</button>
           </div>
-          <div className="show-map">
-            <Map />
+          <div className="quest-view">
+            {screen === 'clue' &&
+              <div className="clues">
+                <h2>Quest</h2>
+                <p>Your next location is:</p>
+                <p>Crown Inn Pub</p>
+                <div className="answer-input">
+                  {/* <h2>Answer Input</h2> */}
+                  <input>
+                  </input>
+                  <button type="button" className="btn btn-info">SUBMIT ANSWER</button>
+                </div>
+                <div className="start-button">
+                  <button type="button" className="btn btn-success btn-lg" onClick={this.nextStop}>{currentStop === 0 ? 'START' : 'NEXT'}</button>
+                </div>
+              </div>
+            }
+            {screen === 'map' &&
+              <div className="show-map">
+                <Map getBounds={() => null} />
+              </div>
+            }
           </div>
         </div>
-      </div>
       </>
     )
   }
