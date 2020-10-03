b98066ba21fd8a9751b394eed1054b2e76c052a1
dongra02 <dsgraham1@gmail.com>
2020-10-03 11:36:09 +0300

    formatting updated to mirror Find styling

--- frontend/src/components/Quests/QuestCreate.js
+++ frontend/src/components/Quests/QuestCreate.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Header from '../common/Header'
 
 class QuestCreate extends React.Component{
 
@@ -33,20 +34,34 @@ class QuestCreate extends React.Component{
 
   render() {
     return (
-      <form>
-        <div className="form-group">
-          <label htmlFor="name">Name Your Quest</label>
-          <input type="text" className="form-control" id="name" />
+      <>
+        <Header />
+        <h3>Create a New Quest</h3>
+        <div className="create-container">
+          <form className="create-form">
+            <div className="form-group">
+              <input type="text" className="form-control" id="name" placeholder="Give your quest a name"/>
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control" id="name" placeholder="City/Location (pick from map?)"/>
+            </div>
+            <div className="form-group">
+              <select className="form-control" id="exampleFormControlSelect1">
+                <option value=''>Select a Theme</option>
+                {this.themes.map(theme => 
+                  <option key={theme} value={theme}>{theme}</option>
+                )}
+              </select>
+            </div>
+            <div className="form-group">
+              <input type="number" className="form-control" id="name" placeholder="Quest duration (minutes)"/>
+            </div>
+          </form>
+          <div className="create-map">
+            <div>This is going to be a map</div>
+          </div>
         </div>
-        <div class="form-group">
-          <label htmlFor="exampleFormControlSelect1">Choose a Theme</label>
-          <select className="form-control" id="exampleFormControlSelect1">
-            {this.themes.map(theme => 
-              <option key={theme} value={theme}>{theme}</option>
-            )}
-          </select>
-        </div>
-      </form>
+      </>
     )
   }
 }
