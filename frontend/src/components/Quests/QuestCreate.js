ca63c5f52462b31df1121b126bc558a57d1d4f81
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-10-06 20:32:58 +0300

    working on form logic and stop display

--- frontend/src/components/Quests/QuestCreate.js
+++ frontend/src/components/Quests/QuestCreate.js
@@ -40,21 +40,12 @@ class QuestCreate extends React.Component{
     (Math.random() * 360) - 180
   ]
 
-  
-
   handleQuestFormChange = event => {
     const questFormData = {
       ...this.state.questFormData,
       [event.target.name]: event.target.value
     }
     this.setState({ questFormData })
-  }
-
-  handleQuestSubmit = async () => {
-    const location = this.state.stops[0].location
-    const newQuestData = { ...this.state.questFormData, stops: [ ...this.state.stops ], location: location }
-    console.log(newQuestData)
-    await createQuest(newQuestData)
   }
 
   handleStopFormChange = event => {
@@ -65,47 +56,60 @@ class QuestCreate extends React.Component{
     this.setState({ stopFormData })
   }
 
-  handleStopAnswerTypeChange = event => {
-    const stopAnswerType = event.target.value
-    const stopFormData = { ...this.state.stopFormData, answerType: stopAnswerType }
-    this.setState({ stopFormData })
+  handleQuestSubmit = async () => {
+    const location = this.state.stops[0].location
+    const newQuestData = { ...this.state.questFormData, stops: [ ...this.state.stops ], location: location }
+    console.log(newQuestData)
+    await createQuest(newQuestData)
   }
 
-  displayAnswerType = () => {
-    const stopAnsweryType = this.state.stopFormData.answerType
-    return stopAnsweryType === 'Answer'
-  }
-
-  handleStopSubmit = event => {
-    event.preventDefault()
+  submitStop = () => {
     const stops = [ ...this.state.stops ]
-    const stopFormData = { ...this.state.stopFormData }
-    stops.push(stopFormData)
-    this.setState({ stops: [...stops] })
+    const newStop = { ...this.state.stopFormData }
+    stops.push(newStop)
+    const stopFormData = {
+      name: '',
+      clue: '',
+      answerType: 'Answer',
+      answer: '',
+      hint: '',
+      location: { latitude: '', longitude: '' }
+    }
+
+    this.setState({ stops, stopFormData, tabShow: 'stops' })
   }
 
-  selectLocation = (location, details) => {
+  // Fires on picking a result from the geocoder suggestions
+  selectLocation = (location, { place_name: geocoderValue }) => {
     const { latitude, longitude } = location
     const flyTo = { latitude, longitude }
-    const stopLocation = { latitude: latitude, longitude: longitude }
-    const updateStop = { ...this.state.stopFormData, location: stopLocation }
-    this.setState({ flyTo, stopFormData: updateStop, geocoderValue: details.place_name }, () => this.setState({ flyTo: null, geocoderValue: null }))
+    const stopFormData = { ...this.state.stopFormData, location: flyTo }
+    this.setState({ flyTo, stopFormData, geocoderValue }, () => this.setState({ flyTo: null }))
   }
 
   selectTab = (event) => {
-    this.setState({ tabShow: event.target.value })
+    const stopFormData = {
+      name: '',
+      clue: '',
+      answerType: 'Answer',
+      answer: '',
+      hint: '',
+      location: { latitude: '', longitude: '' }
+    }
+    this.setState({ tabShow: event.target.value, stopFormData })
   }
 
-  handleMapStopLocale = async (location) => {
+  // TODO handle a blank result here
+  pickLocationFromMap = async (location) => {
     const localeName = await reverseGeoCode(location)
     const geocoderValue = localeName.data.features[0].place_name
-    const stopFormData = { ...this.state.stopFormData, location: location }
-    this.setState({ stopFormData, geocoderValue }, () => this.setState({ geocoderValue: null }))
+    const stopFormData = { ...this.state.stopFormData, location }
+    this.setState({ stopFormData, geocoderValue })
   }
 
   render() {
 
-    const { questFormData, stopFormData, stops, flyTo, tabShow } = this.state
+    const { questFormData, stopFormData, stops, flyTo, tabShow, geocoderValue } = this.state
 
     const tabStyles = {
       info: { display: tabShow === 'info' ? 'block' : 'none' },
@@ -113,44 +117,46 @@ class QuestCreate extends React.Component{
       addStop: { display: tabShow === 'addStop' || (tabShow === 'stops' && stops.length === 0) ? 'block' : 'none' }
     }
 
+    const stopFormProps = {
+      stopFormData: stopFormData,
+      geocoderValue: geocoderValue,
+      handleChange: this.handleStopFormChange,
+      submitStop: this.submitStop,
+      selectLocation: this.selectLocation,
+      selectTab: this.selectTab
+    }
+
+    const questFormProps = {
+      questFormData: questFormData,
+      handleQuestFormChange: this.handleQuestFormChange,
+      handleQuestSubmit: this.handleQuestSubmit,
+      themes: this.themes
+    }
+
     return (
       <div className="create-quest">
         <BgMap latLng={this.bgLatLng} />
         <h3>Create a New Quest</h3>
-        <div>
-          <div className="create-container">
-            <div className="create-info">
-              <div className="show-tabs">
-                <button value={'info'} onClick={this.selectTab} className={`tab ${tabShow === 'info' ? '' : 'inactive'}`} >INFO</button>
-                <button value={'stops'} onClick={this.selectTab} className={`tab ${tabShow === 'stops' || tabShow === 'addStop' ? '' : 'inactive'}`} >STOPS</button>
-              </div>
-              <div className="create-tab" style={tabStyles.info}>
-                <QuestForm
-                  questFormData={questFormData}
-                  handleQuestFormChange={this.handleQuestFormChange}
-                  handleQuestSubmit={this.handleQuestSubmit}
-                  themes={this.themes}
-                />
-              </div>
-              <div className="create-tab" style={tabStyles.addStop}>
-                <StopForm
-                  stopFormData={stopFormData}
-                  geocoderValue={this.state.geocoderValue}
-                  handleStopFormChange={this.handleStopFormChange}
-                  handleStopAnswerTypeChange={this.handleStopAnswerTypeChange}
-                  displayAnswerType={this.displayAnswerType}
-                  handleStopSubmit={this.handleStopSubmit}
-                  selectLocation={this.selectLocation}
-                  selectTab={this.selectTab}
-                />
-              </div>
-              <div className="create-tab" style={tabStyles.stops}>
-                <StopList stops={stops} changeTab={this.selectTab} />
-              </div>
+        <div className="create-container">
+          <div className="create-info">
+            <div className="show-tabs">
+              <button value={'info'} onClick={this.selectTab} className={`tab ${tabShow === 'info' ? '' : 'inactive'}`} >INFO</button>
+              <button value={'stops'} onClick={this.selectTab} className={`tab ${tabShow === 'stops' || tabShow === 'addStop' ? '' : 'inactive'}`} >STOPS</button>
             </div>
-            <div className="create-map">
-              <Map flyTo={flyTo} handleMapStopLocale={this.handleMapStopLocale} />
+            <div className="create-tab" style={tabStyles.info}>
+              <QuestForm {...questFormProps} />
             </div>
+            {/* New Stop Form */}
+            <div className="create-tab" style={tabStyles.addStop}>
+              <StopForm {...stopFormProps} />
+            </div>
+            {/* Stop List */}
+            <div className="create-tab" style={tabStyles.stops}>
+              <StopList stops={stops} changeTab={this.selectTab} />
+            </div>
+          </div>
+          <div className="create-map">
+            <Map flyTo={flyTo} handleMapStopLocale={this.pickLocationFromMap} />
           </div>
         </div>
       </div>
