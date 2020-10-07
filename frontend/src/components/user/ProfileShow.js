ffc1727035dc83fd19d11a3323dea6c926826c6c
Liam Callaghan <ljcallaghan64@gmail.com>
2020-10-07 16:36:00 +0100

    profile revamp

--- frontend/src/components/user/ProfileShow.js
+++ frontend/src/components/user/ProfileShow.js
@@ -2,11 +2,17 @@ import React from 'react'
 import { getSingleProfile } from '../../lib/api'
 import { Link } from 'react-router-dom'
 import { isAuthenticated } from '../../lib/auth'
+import BgMap from '../map/BgMap'
 
 class ProfileShow extends React.Component {
   state = {
     profile: ''
   }
+
+  bgLatLng = [
+    (Math.random() * 180) - 90,
+    (Math.random() * 360) - 180
+  ]
 
   componentDidMount = async () => {
     console.log(this.props.match.params.id)
@@ -21,6 +27,7 @@ class ProfileShow extends React.Component {
     if ( !this.state.profile ) return null
     return (
       <div className='profile'>
+        <BgMap latLng={this.bgLatLng} />
         <table className='table'>
           <thead>
             <tr>
@@ -29,34 +36,23 @@ class ProfileShow extends React.Component {
               <th scope="col">Quests</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-            <div><img src={this.state.profile.imageUrl} alt='Profile image' /></div>
-              <th>Email</th>
-              <td>{this.state.profile.email}</td>
-            </tr>
-            <tr>
-              <th>Username</th>
-              <td>{this.state.profile.username}</td>
-            </tr>
-            <tr>
-              <th>Created Quests</th>
-              <td>{this.state.profile.createdQuest.length}</td>
-              </tr>
-              {this.state.profile.createdQuest.map((quest, i) => (
-                <div key={i} className='container-quest'>
-                  <Link to={`/quests/${quest.id}`}>
-                    <div className="quest-details">
-                      <div className="detail-name">{quest.name}</div>
-                      <div className="detail-theme">{quest.theme}</div>
-                      <div className="detail-rating">{quest.avgRating}</div>
-                      <br />
-                    </div>
-                  </Link>
-                </div>
-              ))}
-              </tbody>
-              </table>
+          </table>
+        <h1 className='title-text'>{this.state.profile.username}</h1>
+        <div><img src={this.state.profile.imageUrl} alt='Profile image' /></div>
+        <h3>{this.state.profile.email}</h3>
+        <h2>Created Quests:</h2>
+        {this.state.profile.createdQuest.map((quest, i) => (
+          <div key={i} className='container-quest'>
+            <Link to={`/quests/${quest.id}`}>
+              <div className="quest-details">
+                <div className="detail-name">{quest.name}</div>
+                <div className="detail-theme">{quest.theme}</div>
+                <div className="detail-rating">{quest.avgRating}</div>
+                <br />
+              </div>
+            </Link>
+          </div>
+        ))}
       </div>
     )
   }
