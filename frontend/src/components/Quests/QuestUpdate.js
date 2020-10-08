6a92469e6c5f6450c98c98862abde2f1530b6c61
dongra02 <dsgraham1@gmail.com>
2020-10-08 10:23:24 +0300

    removed console logs questUpdate

--- frontend/src/components/Quests/QuestUpdate.js
+++ frontend/src/components/Quests/QuestUpdate.js
@@ -4,7 +4,6 @@ import QuestCreate from './QuestCreate'
 const QuestUpdate = (props) => {
 
   const questId = props.match.params.id
-  console.log(questId)
 
   return (
     <QuestCreate questId={questId} />
